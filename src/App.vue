1d1f5511baacb33b9fdfa566261094de5de2b48e
Piotr Glodek <pioglodek@gmail.com>
2021-04-03 17:23:09 +0200

    Add styling to todo app

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,37 @@
 <template>
-  <h1>DC heroes {{ heroesCount }}</h1>
-  <ul>
-    <li v-for="(hero, index) in dcHeroes" :key="hero.name">
-      <p>{{ hero.name }} <button @click="deleteHero(index)">X</button></p>
-    </li>
-  </ul>
-  <form @submit.prevent="addHero">
-    <input v-model.trim="newHero" />
-    <button type="submit">Add hero</button>
-  </form>
+  <div class="w-full flex">
+    <div class="m-auto">
+      <h1 class="text-center text-2xl">DC heroes {{ heroesCount }}</h1>
+      <ul class="mt-3">
+        <li
+          class="flex justify-between mb-2 text-lg"
+          v-for="(hero, index) in dcHeroes"
+          :key="hero.name"
+        >
+          <p>{{ hero.name }}</p>
+          <button
+            class="w-6 h-6 grid place-items-center hover:text-red-500 hover:bg-red-100 focus:outline-none"
+            @click="deleteHero(index)"
+          >
+            X
+          </button>
+        </li>
+      </ul>
+      <form class="mt-10" @submit.prevent="addHero">
+        <input
+          class="border-2 rounded-sm border-gray-300 mr-2 py-1 px-1 focus:outline-none focus:border-indigo-500 hover:bg-gray-100"
+          placeholder="Type hero name "
+          v-model.trim="newHero"
+        />
+        <button
+          class="px-2 py-1 bg-indigo-500 hover:bg-indigo-700 transition-colors rounded-sm text-white"
+          type="submit"
+        >
+          Add hero
+        </button>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
