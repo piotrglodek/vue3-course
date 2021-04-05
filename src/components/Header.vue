8c98e7e58b5e05927e4c26a89d9b3bb2a10deb20
Piotr Glodek <pioglodek@gmail.com>
2021-04-05 19:33:54 +0200

    Create links in header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,26 @@
 <template>
   <nav class="w-full bg-indigo-500 text-white py-2 px-4">
-    <a href="">DC heroes </a>
+    <router-link
+      class="mr-3 text-xl"
+      v-for="link in links"
+      :to="link.path"
+      :key="link.label"
+      >{{ link.label }}</router-link
+    >
   </nav>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      links: [
+        { path: "/dc-heroes", label: "Dc heroes" },
+        { path: "/calendar", label: "Calendar" },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
