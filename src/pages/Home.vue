2bf609c5615bdf5506d503118cff3f08098ec324
Piotr Glodek <pioglodek@gmail.com>
2021-04-05 19:33:38 +0200

    Create Home page

---
+++ src/pages/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="m-auto flex mt-12">
+    <h1 class="text-center">Welcome to full vue 3 course</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
