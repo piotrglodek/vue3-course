305f4bac03605e9694687e2a1640c93a7a1878e2
Piotr Glodek <pioglodek@gmail.com>
2021-04-05 09:22:41 +0200

    Add  Calendar to App

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <Header />
   <div class="w-full flex">
     <!-- <Heroes /> -->
+    <Calendar />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
 // import Heroes from "./components/Heroes";
+import Calendar from "./components/Calendar";
 
 export default {
   components: {
     Header,
     // Heroes,
+    Calendar,
   },
 };
 </script>
